f3a799a7a71364ec55a9e576f21e0a02212e6fb9
Student <student@macbook-pro-0435-luke-tao.local>
2017-02-09 10:02:10 -0500

    finished

--- Mountains/Mountains.playground/Pages/Goal.xcplaygroundpage/Contents.swift
+++ Mountains/Mountains.playground/Pages/Goal.xcplaygroundpage/Contents.swift
@@ -19,19 +19,13 @@ let canvas = Canvas(width: 300, height: 300)
 // Draw some lines// background
 
 canvas.defaultLineWidth = 1
-canvas.drawEllipse(centreX: 0, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 25, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 50, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 75, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 100, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 125, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 150, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 175, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 200, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 225, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 250, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 275, centreY: 300, width: 2, height: 2)
-canvas.drawEllipse(centreX: 300, centreY: 300, width: 2, height: 2)
+
+//draw the dots
+for x in stride(from: 0, through: 300, by: 25){
+    canvas.drawEllipse(centreX: x, centreY: 300, width: 2, height: 2)
+}
+
+//second line
 canvas.drawEllipse(centreX: 0, centreY: 275, width: 2, height: 2)
 canvas.drawEllipse(centreX: 25, centreY: 275, width: 2, height: 2)
 canvas.drawEllipse(centreX: 50, centreY: 275, width: 2, height: 2)
@@ -45,6 +39,8 @@ canvas.drawEllipse(centreX: 225, centreY: 275, width: 2, height: 2)
 canvas.drawEllipse(centreX: 250, centreY: 275, width: 2, height: 2)
 canvas.drawEllipse(centreX: 275, centreY: 275, width: 2, height: 2)
 canvas.drawEllipse(centreX: 300, centreY: 275, width: 2, height: 2)
+
+//third line
 canvas.drawEllipse(centreX: 0, centreY: 250, width: 2, height: 2)
 canvas.drawEllipse(centreX: 25, centreY: 250, width: 2, height: 2)
 canvas.drawEllipse(centreX: 50, centreY: 250, width: 2, height: 2)
@@ -58,6 +54,7 @@ canvas.drawEllipse(centreX: 225, centreY: 250, width: 2, height: 2)
 canvas.drawEllipse(centreX: 250, centreY: 250, width: 2, height: 2)
 canvas.drawEllipse(centreX: 275, centreY: 250, width: 2, height: 2)
 canvas.drawEllipse(centreX: 300, centreY: 250, width: 2, height: 2)
+
 canvas.drawEllipse(centreX: 0, centreY: 225, width: 2, height: 2)
 canvas.drawEllipse(centreX: 25, centreY: 225, width: 2, height: 2)
 canvas.drawEllipse(centreX: 50, centreY: 225, width: 2, height: 2)
@@ -101,10 +98,31 @@ canvas.drawEllipse(centreX: 50, centreY: 150, width: 2, height: 2)
 canvas.drawEllipse(centreX: 150, centreY: 150, width: 2, height: 2)
 canvas.drawEllipse(centreX: 250, centreY: 150, width: 2, height: 2)
 
-//rectangles.
-RGBColor.fillColor Color(
-canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 100, width: 300, height: 50)
+//rectangles 1.
+canvas.drawShapesWithBorders = false
+canvas.fillColor = Color(hue: 0, saturation: 000, brightness: 70, alpha: 74)
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 75, width: 300, height: 75)
 
-//: ## Template code
-//: The code below is necessary to see the result of your work in the Assistant Editor at right. Please do not remove.
+//triangles first rectangle
+for j in 0...2 {
+    for i in stride(from: 0, through: 49, by: 1){
+        canvas.lineColor = Color(hue: 0, saturation: 000, brightness: 70, alpha: 100)
+        canvas.drawLine(fromX: i + j * 100, fromY: 150, toX: i + j * 100, toY: 200 - i)
+        canvas.drawLine(fromX: 50 + i + j * 100, fromY: 150, toX: 50 + i + j * 100, toY: 150 + i)
+
+    }
+}
+//rectangle 2
+canvas.drawShapesWithBorders = false
+canvas.fillColor = Color(hue: 0, saturation: 000, brightness: 140, alpha: 74)
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 300, height: 75)
+//triangles in rectangle 2
+for c in 0...6 {
+    for b in stride(from: 0, through: 24, by: 1){
+        canvas.lineColor = Color(hue: 0, saturation: 000, brightness: 140, alpha: 74)
+        canvas.drawLine(fromX: b + c * 50, fromY: 75, toX: b + c * 50, toY: 100 - b)
+        canvas.drawLine(fromX: 25 + b + c * 50, fromY: 75, toX: 25 + b + c * 50, toY: 75 + b)
+    }
+}
+
 PlaygroundPage.current.liveView = canvas.imageView
